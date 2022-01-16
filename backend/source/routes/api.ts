916473f024ecb1245976dd4dfd813fd2bd7a2b3e
AlexVanLeyen <alex.vanleyen@gmail.com>
2022-01-16 23:30:49 +0800

    fix: corrected methods on api endpoints
    Switched reset to put (was post) and tick to post (was put).

--- backend/source/routes/api.ts
+++ backend/source/routes/api.ts
@@ -5,7 +5,7 @@ import {bookingSchema} from '#schemas/booking';
 const router = express.Router();
 
 router.post('/book', bookingSchema, api.book);
-router.post('/reset', api.reset);
-router.put('/tick', api.tick);
+router.put('/reset', api.reset);
+router.post('/tick', api.tick);
 
 export default router;
