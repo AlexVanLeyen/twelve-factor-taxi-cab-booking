161fe10a68a1c60528307a97e0e38e135c784514
AlexVanLeyen <alex.vanleyen@gmail.com>
2022-01-17 15:33:13 +0800

    feat(swagger): :memo: Added swagger api documentation

--- backend/source/routes/api.ts
+++ backend/source/routes/api.ts
@@ -4,8 +4,89 @@ import {bookingSchema} from '#schemas/booking';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * tags:
+ *  name: api
+ *  description: The taxi booking api
+ */
+
+/**
+ * @openapi
+ * /api/book:
+ *  post:
+ *      description: Attempt to book a taxi within the booking system.
+ *      tags: [api]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          source:
+ *                              type: object
+ *                              properties:
+ *                                  x:
+ *                                      type: integer
+ *                                      example: 100
+ *                                  y:
+ *                                      type: integer
+ *                                      example: -200
+ *                              destination:
+ *                                  type: object
+ *                                  properties:
+ *                                      x:
+ *                                          type: integer
+ *                                          example: -2500
+ *                                      y:
+ *                                          type: integer
+ *                                          example: 50
+ *      responses:
+ *          200:
+ *              description: Booking was successful.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              car_id:
+ *                                  type: integer
+ *                              total_time:
+ *                                  type: integer
+ *          202:
+ *              description: Booking was accepted, however no taxis are available.
+ *          400:
+ *              description: Request was malformed.
+ *          422:
+ *              description: Request was well formed, however the system rejected it.
+ *          500:
+ *              description: (temporary) server error. Try again, or contact support.
+ */
 router.post('/book', bookingSchema, api.book);
+
+/**
+ * @openapi
+ * /api/reset:
+ *  put:
+ *      description: Will reset the taxi booking system.
+ *      tags: [api]
+ *      responses:
+ *          200:
+ *              description: Reset was successful
+ */
 router.put('/reset', api.reset);
+
+/**
+ * @openapi
+ * /api/tick:
+ *  post:
+ *      description: Will advance the simulated taxis by one tick.
+ *      tags: [api]
+ *      responses:
+ *          200:
+ *              description: Request was successful
+ */
 router.post('/tick', api.tick);
 
 export default router;
--- backend/source/server.ts
+++ backend/source/server.ts
@@ -2,17 +2,26 @@ import express from 'express';
 import apiRoutes from '#routes/api';
 import errorRoutes from '#routes/_404';
 import logger, {expressLogger} from '#logger';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import * as swaggerDefinition from 'swagger.json';
+
 
 const PORT = process.env.PORT ?? 8080;
+const specs = swaggerJsdoc({
+    swaggerDefinition,
+    apis: [`**/routes/*.ts`]
+});
 
 const app = express();
 
 app.use(expressLogger);
 app.use(express.json());
 
+app.use(swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/api', apiRoutes);
 app.use(errorRoutes);
 
 app.listen(PORT, () => {
-    logger.info(`Server is running at https://localhost:${PORT}`);
+    logger.info(`Server is running at http://localhost:${PORT}`);
 });
