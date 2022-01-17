63cd63d29cbb05a113064f59f875ff01220220d0
AlexVanLeyen <alex.vanleyen@gmail.com>
2022-01-17 17:14:03 +0800

    refactor: Renamed world.ts to main.ts to improve clarity on purpose.

--- backend/source/main.ts
+++ backend/source/main.ts
@@ -1,3 +1,10 @@
+/**
+ * main.ts
+ *
+ * This is the entry point for the taxi booking service.
+ * This module initializes the service's world engine.
+ */
+
 import Car from '#models/Car';
 import Grid from '#models/Grid';
 import Vertex from '#models/Vertex';
@@ -12,18 +19,23 @@ const grid = new Grid({topLeft, bottomRight});
 
 let world: World;
 
-function init(): void {
+function _init(): void {
     world = new World({grid});
     world.addCar(new Car({id: 1}));
     world.addCar(new Car({id: 2}));
     world.addCar(new Car({id: 3}));
 }
 
+/**
+ * Reinitializes the world. All car positions will be
+ * reset back to the center of the map, and all active
+ * bookings will be dropped.
+ */
 function resetWorld(): void {
-    init();
+    _init();
 }
 
-init();
+_init();
 
 export {
     world,
