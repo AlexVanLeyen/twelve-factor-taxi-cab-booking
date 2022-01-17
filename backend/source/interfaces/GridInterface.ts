c82083e724cdb1386126091e4f206e6daf237e2d
AlexVanLeyen <alex.vanleyen@gmail.com>
2022-01-17 17:15:15 +0800

    docs: :memo: Added docs to interfaces

--- backend/source/interfaces/GridInterface.ts
+++ backend/source/interfaces/GridInterface.ts
@@ -1,5 +1,9 @@
 import Vertex from "#models/Vertex";
-
+/**
+ * The grid interface was added to support expansion into different types
+ * of grids (or graphs). Example: instead of using a 2D grid, what if we
+ * wanted to use a 3D grid?
+ */
 interface GridInterface {
     isValidVertex(vertex: Vertex): boolean;
 }
--- backend/source/interfaces/TickInterface.ts
+++ backend/source/interfaces/TickInterface.ts
@@ -1,3 +1,11 @@
+/**
+ * The tick interface provides the methods required for handling
+ * world ticks.
+ *
+ * This was an experimental idea on using an asynchronous event
+ * driven system to handle world ticks. Ran out of time before
+ * it could be properly implemented.
+ */
 interface TickInterface {
     onTick(): void
 }
